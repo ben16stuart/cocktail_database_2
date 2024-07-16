13018defd9b904f682249ee5806f0f5be29d3390
Ben <104168127+ben16stuart@users.noreply.github.com>
2024-07-15 20:26:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
+from supabase import create_client, Client
+from dotenv import load_dotenv
 import os
-import supabase
+
+load_dotenv()  # Load environment variables from .env file
 
 app = Flask(__name__)
 
@@ -8,29 +11,29 @@ app = Flask(__name__)
 app.config['SUPABASE_URL'] = os.getenv('SUPABASE_URL')
 app.config['SUPABASE_KEY'] = os.getenv('SUPABASE_KEY')
 
-supabase_client = supabase.create_client(app.config['SUPABASE_URL'], app.config['SUPABASE_KEY'])
+supabase_client = create_client(app.config['SUPABASE_URL'], app.config['SUPABASE_KEY'])
 
 @app.route('/drinks', methods=['GET', 'POST'])
 def drinks():
     if request.method == 'POST':
         data = request.json
-        response = supabase.table('drinks').insert(data).execute()
+        response = supabase_client.table('drinks').insert(data).execute()
         return jsonify(response.data), 201
     else:
-        response = supabase.table('drinks').select('*').execute()
+        response = supabase_client.table('drinks').select('*').execute()
         return jsonify(response.data), 200
 
 @app.route('/drinks/<int:id>', methods=['GET', 'PUT', 'DELETE'])
 def drink(id):
     if request.method == 'GET':
-        response = supabase.table('drinks').select('*').eq('id', id).execute()
+        response = supabase_client.table('drinks').select('*').eq('id', id).execute()
         return jsonify(response.data), 200
     elif request.method == 'PUT':
         data = request.json
-        response = supabase.table('drinks').update(data).eq('id', id).execute()
+        response = supabase_client.table('drinks').update(data).eq('id', id).execute()
         return jsonify(response.data), 200
     elif request.method == 'DELETE':
-        response = supabase.table('drinks').delete().eq('id', id).execute()
+        response = supabase_client.table('drinks').delete().eq('id', id).execute()
         return jsonify(response.data), 204
 
 if __name__ == '__main__':
